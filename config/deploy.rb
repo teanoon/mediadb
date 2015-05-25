bac349beffbd4e5b4fa29d542f54103a801faee0
teanoon <anticipationvu@gmail.com>
2015-05-25 10:54:07 +0800

    use http / set deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -8,7 +8,7 @@ set :repo_url, 'git@github.com:teanoon/mediadb.git'
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
 
 # Default deploy_to directory is /var/www/my_app_name
-# set :deploy_to, '/var/www/my_app_name'
+set :deploy_to, '/home/deploy/apps/mediadb'
 
 # Default value for :scm is :git
 # set :scm, :git
